186bf4aee6ae76704b7816fd93a1af3b26f0cd4d
MakramElghadir <MkElghadir@gmail.com>
2024-11-04 09:18:18 +0100

    preparacion de servidor.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express")
+const {exec} = require('child process')
+
+const commando = 'Hola figlet'
 const app = express()
 const port = 3000
 
 app.get("/", (req,res) => {
-    req.send("holla")
+    exec(command,(error,stdout,stderr) => {
+        console.log(error, stdout, stderr)
+        console.log(`Stdout: ${stdout}`)
+        req.send(stdout)
+    })
 })
 
 app.listen(port, () => {
-    
+    console.log(`servidor ${port} funciona`)
 })
