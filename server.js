e417998be5dd2773b3a9ab3963ce8c2cc428cd61
MakramElghadir <MkElghadir@gmail.com>
2024-11-04 10:51:02 +0100

    incluso de HTML

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require("express")
-const {exec} = require('child process')
+const { exec } = require('child_process');
 
-const commando = 'Hola figlet'
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
+
 app.get("/", (req,res) => {
-    exec(command,(error,stdout,stderr) => {
-        console.log(error, stdout, stderr)
-        console.log(`Stdout: ${stdout}`)
-        req.send(stdout)
-    })
+    exec(comando, (error, stdout, stderr) => {
+        res.send(`<pre>${stdout}</pre>`)
+      });
+})
+
+app.get("/ping", (req, res) => {
+    const dominio = req.query.dominio
+    const comando = "ping -c 4 " + dominio
+    exec(comando, (error, stdout, stderr) => {
+        res.send(stdout)
+    });
 })
 
 app.listen(port, () => {
-    console.log(`servidor ${port} funciona`)
+    console.log(`${port} open`)
 })
